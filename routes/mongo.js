9c2dc761a707e17f78766ff1bd9bf419628d6560
Sara <jjynxx@gmail.com>
2023-12-01 11:33:32 -0500

    List collections

--- routes/mongo.js
+++ routes/mongo.js
@@ -6,9 +6,21 @@ const mongoClient = require("../config/db.js")
 // localhost:3000/db/
 
 router.get("/", (req, res) => {
-    res.send("<h1>Your Name Goes Here</h1>")
+    res.send("<h1>Sara McHattie</h1>")
 })
 
 // TODO Add a "/collections" route handler that lists the collections available in the sample_mflix database
+
+router.get("/collections", async (req, res) => {
+    let connection = await mongoClient.connect()
+    let db = await connection.db("sample_mflix")
+    let collections = await db.listCollections().toArray()
+
+    let data = {
+        collections: collections,
+    }
+
+    res.render("collections", data)
+})
 
 module.exports = router
