39e0c45fb574c9948c2f83d761428a316080b672
Sara <jjynxx@gmail.com>
2023-12-15 18:25:48 -0500

    add queries

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,20 @@ router.get("/", function (req, res, next) {
     })
 })
 
+
+// search page request function
+router.post("/search", async (req, res) => {
+    try {
+        const searchTitle = req.body.title; // Assuming you have a form input with the name "title"
+        let movies = await movieCollection.find({ title: { $regex: searchTitle, $options: "i" } }).toArray();
+        let data = {
+            movies: movies,
+        };
+        res.render("movies", data);
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 module.exports = router
--- routes/mongo.js
+++ routes/mongo.js
@@ -1,27 +1,26 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const mongoClient = require("../config/db.js");
 
-const mongoClient = require("../config/db.js")
-
+// By Sara McHattie
 // localhost:3000/db/
-
 router.get("/", (req, res) => {
-    res.send("<h1>Sara McHattie</h1>")
-})
+    res.send("<h1>Sara McHattie</h1>");
+});
 
-// TODO Add a "/collections" route handler that lists the collections available in the sample_mflix database
 
 router.get("/collections", async (req, res) => {
-    let connection = await mongoClient.connect()
-    let db = await connection.db("sample_mflix")
-    let collections = await db.listCollections().toArray()
+    // Connect to the MongoDB server and retrieve the list of collections in the sample_mflix database
+    let connection = await mongoClient.connect();
+    let db = await connection.db("sample_mflix");
+    let collections = await db.listCollections().toArray();
 
     let data = {
         collections: collections,
-    }
+    };
 
-    res.render("collections", data)
-})
+    res.render("collections", data);
+});
 
 router.get("/movies", async (req, res) => {
     try {
@@ -29,10 +28,32 @@ router.get("/movies", async (req, res) => {
         let db = await connection.db("sample_mflix");
         let movieCollection = db.collection("movies");
 
-        let movies = await movieCollection.find().limit(5).toArray();
+        // Fetch all unique genres and languages from the collection
+        let genres = await movieCollection.distinct("genres");
+        let languages = await movieCollection.distinct("languages");
+
+        let title = req.query.title;  // Get the title from the query parameters
+        let selectedGenre = req.query.genres;
+
+        let query = {};
+
+        if (title) {
+            // If a title is provided, use a case-insensitive regular expression to match
+            query.title = { $regex: title, $options: "i" };
+        }
+
+        if (selectedGenre) {
+            // If a genre is selected, filter movies by that genre
+            query.genres = selectedGenre;
+        }
+
+        // Fetch movies based on the constructed query, limit to 5 results
+        let movies = await movieCollection.find(query).limit(5).toArray();
 
         let data = {
             movies: movies,
+            genres: genres,
+            languages: languages,  // Include the languages array
         };
 
         res.render("movies", data);
@@ -41,25 +62,5 @@ router.get("/movies", async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
-/*
-    let data = {
-        movies: movies,
-    }
 
-    res.render("movies", data)
-    */
-
-
-
-
-/*
-    let data = {
-        movies: movies,
-    }
-
-    res.render("movies", data)
-    */
-
-
-
-module.exports = router
+module.exports = router;
